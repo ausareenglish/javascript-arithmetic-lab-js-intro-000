74c7c126627c15d880680982c03d9cde7ad8fe16
Tehuti English <ausareenglish@gmail.com>
2020-03-18 06:04:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,6 +18,13 @@ function divide(a, b) {
   
 }
 
+function increment(n) {
+  return n += 1;
+}
+
+function decrement(n) {
+  return n -= 1;
+}
 function makeInt(string) {
   return parseInt(string, 10);
 
